b7cf5187fae6be14318c2ec63e36748513e84f1d
Tobias Scavenius <scaveothesanguine@gmail.com>
2015-12-14 15:06:02 +0100

    customer test should work as intended now, it checks wether or not customer throws exceptions correctly

--- src/model/CustomerTest.java
+++ src/model/CustomerTest.java
@@ -1,71 +1,69 @@
 package model;
 
 import exceptions.CustomerException;
-import org.junit.Rule;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Tobias Scavenius on 14-12-2015.
  */
 public class CustomerTest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Test(expected = CustomerException.class)
+    @Test
     public void testThrowsExceptionOnIncorrectName() {
-
-        thrown.expect(new  CustomerException());
-        Customer testCustomer1 = new Customer("1337 H4x0r","51785 962");
-
-    }
-
-    @Test(expected = CustomerException.class)
-    public void testThrowsExceptionOnCorrectName() {
-
-        Customer testCustomer2 = new Customer("Emil Hansen","51 78 56 32");
-
-    }
-
-    @Test(expected = CustomerException.class)
-    public void testThrowsExceptionOnEmptyName() {
-
-        Customer testCustomer3 = new Customer("","");
-
-    }
-
-    @Test(expected = CustomerException.class)
-    public void testThrowsExceptionOnPerfectName() {
-
-        Customer testCustomer0 = new Customer("Emil Hansen","51785632");
-
-    }
-
-    // method recreated because private classes cannot be tested remotely
-    private boolean assertIsName(Customer customer) {
-
-        return !customer.getName().matches(".*[a-zA-z].*") && !customer.getName().matches(".*[a-zA-z].*");
-
-    }
-
-    @org.junit.Test
-    public void testGetNumber() throws Exception {
 
         try {
 
-            //Make the necessary components
-            Customer testCustomer0 = new Customer("Emil Hansen","51785632");
-            Customer testCustomer1 = new Customer("1337 H4x0r","51785 962");
-            Customer testCustomer2 = new Customer("Emil Hansen","51 78 56 32");
-            Customer testCustomer3 = new Customer("","");
+            Customer testCustomer1 = new Customer("1337 H4x0r","51785962");
+            Assert.fail();
 
         } catch (CustomerException e) {
 
         }
 
+    }
+
+
+    @Test
+    public void testShouldNotThrowExceptionOnCorrectNameAndNumberWithSpaces() {
+
+        try {
+
+            Customer testCustomer2 = new Customer("Emil Hansen","51 78 56 32");
+
+        } catch (CustomerException e) {
+            //assertThat(e);
+        }
+
+
+    }
+
+    @Test
+    public void testThrowsExceptionOnEmptyName() {
+
+
+        try {
+
+            Customer testCustomer3 = new Customer("","");
+            Assert.fail();
+
+        } catch (CustomerException e) {
+
+        }
+
+
+    }
+
+    @Test
+    public void testShouldNotThrowExceptionOnPerfectName() {
+
+        try {
+
+            Customer testCustomer0 = new Customer("Emil Hansen","51785632");
+
+        } catch (CustomerException e) {
+
+        }
 
     }
 
