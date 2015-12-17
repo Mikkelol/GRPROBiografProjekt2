1d957404198054c38eb1884ee843e6dabad425ef
Olaleo <aksel@jessens.org>
2015-12-17 13:05:47 +0100

    Finished ReservationTest

--- src/model/TheaterTest.java
+++ src/model/TheaterTest.java
@@ -38,6 +38,7 @@ public class TheaterTest {
             fail();
         }
     }
+
     @Test
     public void testShouldThrowExceptionOnCorrectRowsAndIncorrectColumnsOfValueZero() throws Exception
     {
